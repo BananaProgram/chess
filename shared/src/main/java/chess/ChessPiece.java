07afc0c36ccb07e3d464c5797d1c28c7b11f02ce
Elena Ketcheson <elenakayketch@gmail.com>
2025-05-03 13:01:44 -0600

    Fixed pieceMoves method for queens

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -172,16 +172,8 @@ public class ChessPiece {
         Vector<ChessMove> possibleMoves = new Vector<>();
 
         if (this.type == PieceType.QUEEN) {
-            for (int i = 1; i < 9; i++) {
-                for (int j = 1; j < 9; j++) {
-                    if ((i == myPosition.getRow()) || (j == myPosition.getColumn())) {
-                        ChessPosition endPosition = new ChessPosition(i, j);
-                        if (board.getPiece(endPosition) == null) {
-                            possibleMoves.addElement(new ChessMove(myPosition, endPosition, null));
-                        }
-                    }
-                }
-            }
+            possibleMoves.addAll(checkDiagonals(myPosition, board));
+            possibleMoves.addAll(checkAxes(myPosition, board));
         } else if (this.type == PieceType.ROOK) {
             return possibleMoves;
         } else if (this.type == PieceType.BISHOP) {
