a067e553c36c7740ba890cd48593101c54a25335
Elena Ketcheson <elenakayketch@gmail.com>
2025-05-20 20:34:23 -0600

    added listGames method

--- server/src/main/java/dataaccess/MemoryDataAccess.java
+++ server/src/main/java/dataaccess/MemoryDataAccess.java
@@ -1,13 +1,20 @@
 package dataaccess;
 
 import model.AuthData;
+import model.GameData;
 import model.UserData;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 import java.util.HashMap;
 
 public class MemoryDataAccess implements DataAccess {
     private HashMap<String, UserData> users = new HashMap<>();
     private HashMap<String, String> authTokens = new HashMap<>();
+    private HashMap<Integer, GameData> games = new HashMap<>();
+
+    private int gameID = 0;
 
     public void clear() {
         users = new HashMap<>();
@@ -40,5 +47,15 @@ public class MemoryDataAccess implements DataAccess {
 
     public void deleteAuth(String authData) {
         authTokens.remove(authData);
+    }
+
+    public List<GameData> listGames() {
+        List<GameData> gameList = new ArrayList<>();
+
+        for (int i = 0; i < gameID; i++) {
+            gameList.add(games.get(i));
+        }
+
+        return gameList;
     }
 }
