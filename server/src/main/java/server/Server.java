8d78658d87b790d43995adc1eb19cba20a4ce1a6
Elena Ketcheson <elenakayketch@gmail.com>
2025-05-20 19:18:23 -0600

    implemented clear and register endpoints

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,8 +1,20 @@
 package server;
 
+import dataaccess.DataAccessException;
+import dataaccess.MemoryDataAccess;
+import model.*;
+import service.RegisterRequest;
+import service.UserService;
+import spark.Request;
+import spark.Response;
 import spark.Spark;
+import com.google.gson.Gson;
 
 public class Server {
+    private final UserService userService = new UserService(new MemoryDataAccess());
+
+    public Server() {
+    }
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
@@ -14,6 +26,9 @@ public class Server {
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
 
+        Spark.post("/user", this::register);
+        Spark.delete("/db", this::clear);
+
         Spark.awaitInitialization();
         return Spark.port();
     }
@@ -21,5 +36,17 @@ public class Server {
     public void stop() {
         Spark.stop();
         Spark.awaitStop();
+    }
+
+    private Object register(Request req, Response res) throws DataAccessException {
+        var user = new Gson().fromJson(req.body(), RegisterRequest.class);
+        var result = userService.register(user);
+        return new Gson().toJson(result);
+    }
+
+    private Object clear(Request req, Response res) {
+        userService.clear();
+        res.status(200);
+        return "{}";
     }
 }
