0c14b31960ea48e7cb8db13885a754bababc7414
Elena Ketcheson <elenakayketch@gmail.com>
2025-05-20 20:11:31 -0600

    Added getUser, createAuth, and deleteAuth methods

--- server/src/main/java/dataaccess/MemoryDataAccess.java
+++ server/src/main/java/dataaccess/MemoryDataAccess.java
@@ -21,9 +21,24 @@ public class MemoryDataAccess implements DataAccess {
     public AuthData addUser(UserData user) {
         user = new UserData(user.username(), user.password(), user.password());
         String token = generateToken();
+
         AuthData authData = new AuthData(token, user.username());
-        users.put(token, user);
         authTokens.put(token, user.username());
         return authData;
+    }
+
+    public UserData getUser(String username) {
+        return users.get(username);
+    }
+
+    public AuthData createAuth(String username) {
+        String token = generateToken();
+        AuthData authData = new AuthData(token, username);
+        authTokens.put(token, username);
+        return authData;
+    }
+
+    public void deleteAuth(String authData) {
+        authTokens.remove(authData);
     }
 }
